a521b8298d9c314b80a658d796d9abf5e8e8be96
jerrydoubles <jerrydoubles@gmail.com>
2023-02-21 08:16:21 -0800

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry point
+ * main - all natural numbers below 13
  *
- * Return: Always 0 (Success)
+ *
+ * Return: 0
  */
 int main(void)
 {
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
