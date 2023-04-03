272a46873b026d3bec043ba029191215fe2b3575
jerrydoubles <jerrydoubles@gmail.com>
2023-04-03 14:41:27 -0700

    data structure

---
+++ 0x13-more_singly_linked_lists/data_struct.c
@@ -0,0 +1,16 @@
+#include <stdio>
+#include "lists.h"
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
