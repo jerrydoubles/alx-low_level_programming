d9acb28640f4559a861470f8a71f200c85e4cdbf
jerrydoubles <jerrydoubles@gmail.com>
2023-04-03 11:59:08 -0700

    0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t numbz = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 	printf("%d\n", h->n);
-	num++;
+	numbz++;
 	h = h->next;
 	}
-	return (num);
+	return (numbz);
 }
