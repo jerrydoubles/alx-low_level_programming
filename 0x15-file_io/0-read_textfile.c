aab0c828ae1ffb547a599ec5946b6313171494d5
jerrydoubles <jerrydoubles@gmail.com>
2023-04-09 17:08:48 -0700

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,8 +3,10 @@
  * Auth: Jerry Doubles
  */
 
-#include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
@@ -14,9 +16,10 @@
  * Return: If the function fails or filename is NULL - 0.
  * O/w - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fd, bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
@@ -24,15 +27,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	return (0);
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 	free(buffer);
 	return (0);
 	}
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
 	free(buffer);
-	close(o);
-	return (w);
+	close(fd);
+	return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+	free(buffer);
+	close(fd);
+	return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (bytes_written);
 }
