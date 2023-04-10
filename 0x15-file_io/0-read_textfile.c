7a138869cfa3882e9a244eb6c06a16da69867abd
jerrydoubles <jerrydoubles@gmail.com>
2023-04-09 19:29:52 -0700

    readme edit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,14 +13,14 @@
  * @letters: The number of letters it should read and print
  *
  * Return: The actual number of letters it could read and print
- *		if the file can not be opened or read, return 0
- *		if filename is NULL return 0
- *		if write fails or does not write the expected amount of bytes, return 0
+ *	if the file can not be opened or read, return 0
+ *	if filename is NULL return 0
+ *	if write fails or does not write the expected amount of bytes, return 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer; /* Buffer to store text */
+	char *buffer;
 	ssize_t fd, my_read, my_write;
 
 	if (!filename)
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,1 +1,3 @@
-0x15-file_io
+Directory: 0x15-file_io
+Author: Jerry Doubles
+Date: 4th April, 2023
